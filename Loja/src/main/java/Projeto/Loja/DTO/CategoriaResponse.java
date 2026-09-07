bf44d6f6c04513fa068320e62a914096b027d0c0
Marco <marcooo0034h@gmail.com>
2026-09-07 10:46:28 -0300

    Feat: Implementa a DTO resquest e response de Categoria

---
+++ Loja/src/main/java/Projeto/Loja/DTO/CategoriaResponse.java
@@ -0,0 +1,17 @@
+package Projeto.Loja.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoriaResponse {
+
+    private Long id;
+
+    private String nome;
+}

51006c9c736e9cd424d49d5df878c144c5a822fb
Marco <marcooo0034h@gmail.com>
2026-09-07 20:14:51 -0300

    Feat: Implementa os metodos toEntity, toResponse e updateEntity em CategoriasMapper

--- Loja/src/main/java/Projeto/Loja/Mapper/CategoriaMapper.java
+++ Loja/src/main/java/Projeto/Loja/Mapper/CategoriaMapper.java
@@ -1,8 +1,32 @@
 package Projeto.Loja.Mapper;
 
-import org.mapstruct.Mapper;
+import Projeto.Loja.DTO.CategoriaRequest;
+import Projeto.Loja.DTO.CategoriaResponse;
+import Projeto.Loja.Entity.Categoria;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CategoriaMapper {
+
+    public Categoria toEntity(CategoriaRequest request) {
+
+        Categoria categoria = new Categoria();
+
+        categoria.setNome(request.getNome());
+
+        return  categoria;
+    }
+
+    public CategoriaResponse toResponse(Categoria categoria) {
+
+        return new CategoriaResponse(
+                categoria.getId(),
+                categoria.getNome()
+        );
+    }
+
+    public void updateEntity(CategoriaRequest request , Categoria categoria) {
+
+        categoria.setNome(request.getNome());
+    }
 }

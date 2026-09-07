00f9c9f80b88a56c0cc3f78cf925e58bf909898f
Marco <marcooo0034h@gmail.com>
2026-09-07 12:09:12 -0300

    Feat: Implementa o CRUD de CategoriaService

--- Loja/src/main/java/Projeto/Loja/Service/CategoriaService.java
+++ Loja/src/main/java/Projeto/Loja/Service/CategoriaService.java
@@ -2,29 +2,60 @@ package Projeto.Loja.Service;
 
 import Projeto.Loja.DTO.CategoriaRequest;
 import Projeto.Loja.DTO.CategoriaResponse;
+import Projeto.Loja.Entity.Categoria;
+import Projeto.Loja.Mapper.CategoriaMapper;
 import Projeto.Loja.Repository.CategoriaRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class CategoriaService {
 
     private final CategoriaRepository categoriaRepository;
+    private final CategoriaMapper categoriaMapper;
 
-    public CategoriaResponse save() {
+    public CategoriaResponse save(CategoriaRequest request) {
 
+        Categoria categoria = categoriaMapper.toEntity(request);
+
+        Categoria categoriaSalva = categoriaRepository.save(categoria);
+
+        return categoriaMapper.toResponse(categoriaSalva);
     }
 
-    public CategoriaResponse findAll () {
+    public List<CategoriaResponse> findAll () {
 
+        List<Categoria> lista = categoriaRepository.findAll();
+
+        return lista.stream()
+                .map(categoriaMapper::toResponse)
+                .toList();
+    }
+    public CategoriaResponse findById(Long id) {
+
+        Categoria categoria = categoriaRepository.findById(id)
+                .orElseThrow();
+
+        return categoriaMapper.toResponse(categoria);
     }
 
-    public CategoriaResponse update () {
+    public CategoriaResponse update (Long id, CategoriaRequest request) {
 
+        Categoria categoria = categoriaRepository.findById(id)
+                .orElseThrow();
+
+        categoriaMapper.updateEntity(request, categoria);
+
+        Categoria categoriaAtualizada = categoriaRepository.save(categoria);
+
+        return categoriaMapper.toResponse(categoriaAtualizada);
     }
 
-    public void delete () {
+    public void delete (Long id) {
 
+        categoriaRepository.deleteById(id);
     }
 }

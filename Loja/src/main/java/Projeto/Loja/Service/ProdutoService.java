5534623f3d12b10a9366eb5cd24ba420e9cbb408
Marco <marcooo0034h@gmail.com>
2026-09-07 19:41:10 -0300

    Feat: Implementa o CRUD de ProdutoService

--- Loja/src/main/java/Projeto/Loja/Service/ProdutoService.java
+++ Loja/src/main/java/Projeto/Loja/Service/ProdutoService.java
@@ -1,31 +1,77 @@
 package Projeto.Loja.Service;
 
-import Projeto.Loja.DTO.ClienteResponse;
+import Projeto.Loja.DTO.ProdutoRequest;
 import Projeto.Loja.DTO.ProdutoResponse;
+import Projeto.Loja.Entity.Categoria;
+import Projeto.Loja.Entity.Produto;
+import Projeto.Loja.Mapper.ProdutoMapper;
+import Projeto.Loja.Repository.CategoriaRepository;
 import Projeto.Loja.Repository.ProdutoRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
+
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class ProdutoService {
 
+    private final CategoriaRepository categoriaRepository;
     private final ProdutoRepository produtoRepository;
+    private final ProdutoMapper produtoMapper;
 
-    public ProdutoResponse save() {
+    public ProdutoResponse save(ProdutoRequest request) {
+
+        Produto produto = produtoMapper.toEntity(request);
+
+        Categoria categoria = categoriaRepository.findById(request.getCategoriaId())
+                .orElseThrow();
+
+        produto.setCategoria(categoria);
+
+        Produto produtoSalvo = produtoRepository.save(produto);
+
+        return produtoMapper.toResponse(produtoSalvo);
 
     }
 
-    public ProdutoResponse findAll () {
+    public List<ProdutoResponse> findAll () {
+
+        List<Produto> lista = produtoRepository.findAll();
+
+        return lista.stream()
+                .map(produtoMapper :: toResponse)
+                .toList();
+    }
+    public ProdutoResponse findById(Long id) {
+
+        Produto produto = produtoRepository.findById(id)
+                .orElseThrow();
+
+        return produtoMapper.toResponse(produto);
 
     }
 
-    public ProdutoResponse update () {
+    public ProdutoResponse update (Long id , ProdutoRequest request) {
+
+        Produto produto = produtoRepository.findById(id)
+                .orElseThrow();
+
+        Categoria categoria = categoriaRepository.findById(request.getCategoriaId())
+                .orElseThrow();
+
+        produtoMapper.updateEntity(request , produto);
+
+        produto.setCategoria(categoria);
+
+        Produto produtoSalvo = produtoRepository.save(produto);
+
+        return produtoMapper.toResponse(produtoSalvo);
 
     }
 
-    public void delete () {
-
+    public void delete (Long id) {
+        produtoRepository.deleteById(id);
     }
 }

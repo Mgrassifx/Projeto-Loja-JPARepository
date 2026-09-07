41a72df594fc9403e0f5db176d2f1f62601e2116
Marco <marcooo0034h@gmail.com>
2026-09-07 20:24:41 -0300

    Feat: Implementa os metodos toEntity , toResponse e updateEntity em ClienteMapper

--- Loja/src/main/java/Projeto/Loja/Mapper/ClienteMapper.java
+++ Loja/src/main/java/Projeto/Loja/Mapper/ClienteMapper.java
@@ -1,8 +1,38 @@
 package Projeto.Loja.Mapper;
 
+import Projeto.Loja.DTO.ClienteRequest;
+import Projeto.Loja.DTO.ClienteResponse;
+import Projeto.Loja.Entity.Cliente;
 import org.mapstruct.Mapper;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ClienteMapper {
+
+    public Cliente toEntity(ClienteRequest request) {
+
+        Cliente cliente = new Cliente();
+
+        cliente.setNome(request.getNome());
+        cliente.setEmail(request.getEmail());
+        cliente.setTelefone(request.getTelefone());
+
+        return cliente;
+    }
+
+    public ClienteResponse toResponse(Cliente cliente) {
+
+        return new ClienteResponse(
+                cliente.getId(),
+                cliente.getNome(),
+                cliente.getEmail(),
+                cliente.getTelefone()
+        );
+    }
+    public void updateEntity(ClienteRequest request , Cliente cliente) {
+
+        cliente.setNome(request.getNome());
+        cliente.setEmail(request.getEmail());
+        cliente.setTelefone(request.getTelefone());
+    }
 }

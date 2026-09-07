e5407aa02ebee6d3c802ab24c8b70bbdb4ed17e0
Marco <marcooo0034h@gmail.com>
2026-09-07 00:22:41 -0300

    Feat: Implementa a entidade Pedido

---
+++ Loja/src/main/java/Projeto/Loja/Entity/Pedido.java
@@ -0,0 +1,33 @@
+package Projeto.Loja.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "pedidos")
+public class Pedido {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id" ,  nullable = false)
+    private Cliente cliente;
+
+    @Column(name = "data_pedido" , nullable = false)
+    private LocalDateTime dataPedido;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status_pedido" , nullable = false)
+    private StatusPedido status ;
+}

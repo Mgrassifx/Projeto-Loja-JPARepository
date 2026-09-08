a0dca75cc6daf6859546e65344a616c131f05f98
Marco <marcooo0034h@gmail.com>
2026-09-07 21:43:39 -0300

    Feat : Implementa os metodos toEntity, toResponse e updateEntity em PedidoService

--- Loja/src/main/java/Projeto/Loja/Mapper/PedidoMapper.java
+++ Loja/src/main/java/Projeto/Loja/Mapper/PedidoMapper.java
@@ -1,8 +1,44 @@
 package Projeto.Loja.Mapper;
 
-import org.mapstruct.Mapper;
+import Projeto.Loja.DTO.ClienteResponse;
+import Projeto.Loja.DTO.PedidoRequest;
+import Projeto.Loja.DTO.PedidoResponse;
+import Projeto.Loja.Entity.Cliente;
+import Projeto.Loja.Entity.Pedido;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PedidoMapper {
+
+    public Pedido toEntity(PedidoRequest request) {
+
+        Pedido pedido = new Pedido();
+
+        pedido.setStatus(request.getStatus());
+
+        return pedido;
+    }
+
+    public PedidoResponse toResponse (Pedido pedido) {
+
+        ClienteResponse cliente = new ClienteResponse(
+                pedido.getCliente().getId(),
+                pedido.getCliente().getNome(),
+                pedido.getCliente().getEmail(),
+                pedido.getCliente().getTelefone()
+        );
+
+        return new PedidoResponse(
+                pedido.getId(),
+                cliente,
+                pedido.getDataPedido(),
+                pedido.getStatus()
+
+        );
+    }
+
+    public void updateEntity(PedidoRequest request , Pedido pedido) {
+
+        pedido.setStatus(request.getStatus());
+    }
 }

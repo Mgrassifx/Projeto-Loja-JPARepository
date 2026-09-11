d91c48e0d21bbc48d8aea27a0fefdccc1d7f37b6
MARCO ANTÔNIO GRASSI <marco_grassi@estudante.sesisenai.org.br>
2026-09-10 21:10:17 -0300

    Fex: implementa a documentação do swagger dentro de Produto e clienteController

--- Loja/src/main/java/Projeto/Loja/Controller/ClienteController.java
+++ Loja/src/main/java/Projeto/Loja/Controller/ClienteController.java
@@ -7,6 +7,8 @@ import Projeto.Loja.DTO.ClienteRequest;
 import Projeto.Loja.DTO.ClienteResponse;
 import Projeto.Loja.Service.CategoriaService;
 import Projeto.Loja.Service.ClienteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,35 +17,56 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/clientes")
+@Tag(name = "Clientes" , description = "Operaçoes relacionadas aos clientes")
 public class ClienteController {
 
 
     private final ClienteService clienteService;
 
     @GetMapping
+    @Operation(
+            summary = "Listar Clientes",
+            description = "Retorna todos os Clientes cadastrados no sistema "
+    )
     public List<ClienteResponse> listAll() {
         return clienteService.findAll();
     }
 
     @GetMapping("/{id}")
+    @Operation(
+            summary = "lista clientes por id",
+            description = "lista um cliente referente ao id"
+    )
     public ClienteResponse findById(@PathVariable Long id) {
 
         return clienteService.findById(id);
     }
 
     @PostMapping
+    @Operation(
+            summary = "cadastra cliente",
+            description = "cadastra cliente dentro do sistema"
+    )
     public ClienteResponse save(@RequestBody ClienteRequest cliente) {
 
         return clienteService.save(cliente);
     }
 
     @PutMapping("/{id}")
+    @Operation(
+            summary = "atualiza cliente",
+            description = "atualiza um cliente do sistema"
+    )
     public ClienteResponse update(@PathVariable Long id, @RequestBody ClienteRequest cliente) {
 
         return clienteService.update(id , cliente);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(
+            summary = "deleta Cliente",
+            description = "Deleta um cliente dentro do sistema"
+    )
     public void delete(@PathVariable Long id) {
 
         clienteService.delete(id);
--- Loja/src/main/java/Projeto/Loja/Controller/ProdutoController.java
+++ Loja/src/main/java/Projeto/Loja/Controller/ProdutoController.java
@@ -7,6 +7,8 @@ import Projeto.Loja.DTO.ProdutoRequest;
 import Projeto.Loja.DTO.ProdutoResponse;
 import Projeto.Loja.Service.CategoriaService;
 import Projeto.Loja.Service.ProdutoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +17,7 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/produtos")
+@Tag(name = "Produtos", description = "Operaçoes relacionadas aos produtos")
 public class ProdutoController {
 
 
@@ -22,29 +25,49 @@ public class ProdutoController {
     private final ProdutoService produtoService;
 
     @GetMapping
+    @Operation(
+            summary = "Listar Produtos",
+            description = "Listar todos os produtos caadastrados no sistema"
+    )
     public List<ProdutoResponse> listAll() {
         return produtoService.findAll();
     }
 
     @GetMapping("/{id}")
+    @Operation(
+            summary = "Lista produto por Id",
+            description = "Lista um produto referente ao id"
+    )
     public ProdutoResponse findById(@PathVariable Long id) {
 
         return produtoService.findById(id);
     }
 
     @PostMapping
+    @Operation(
+            summary = "Cadastrar produto",
+            description = "cadastra um novo produto no sistema"
+    )
     public ProdutoResponse save(@RequestBody ProdutoRequest produto) {
 
         return produtoService.save(produto);
     }
 
     @PutMapping("/{id}")
+    @Operation(
+            summary = "Atualiza Produto",
+            description = "Atualiza um produto no sistema"
+    )
     public ProdutoResponse update(@PathVariable Long id, @RequestBody ProdutoRequest produto) {
 
         return produtoService.update(id , produto);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(
+            summary = "Deleta Produto",
+            description = "Deleta um produto do sistema"
+    )
     public void delete(@PathVariable Long id) {
 
         produtoService.delete(id);
